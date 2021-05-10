e45d35e10e2b76f89e934551a393591f117dc98a
R9 Mobile dev <asamir@road9media.com>
2021-05-11 01:06:00 +0200

    add application navigations

---
+++ src/navigation/HomeNavigation.tsx
@@ -0,0 +1,77 @@
+import { View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { theme } from "../constants";
+import { strings } from "../localization/i18n";
+import HomeScreen from "../screens/Home/Home";
+import MovieDetails from "../screens/MovieDetails/MovieDetails";
+import FavoritesScreen from "../screens/Favorites/Favorites";
+
+const HomeFlow = createStackNavigator();
+
+const HomeNavigation = () => (
+  <HomeFlow.Navigator initialRouteName="Home">
+    <HomeFlow.Screen
+      name="Home"
+      component={HomeScreen}
+      options={({ navigation }) => {
+        return {
+          headerTitle: strings("moviesHome"),
+          headerStyle: {
+            backgroundColor: theme.colors.accent,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: theme.colors.white,
+          },
+          headerRight: () => (
+            <MaterialCommunityIcons
+              name="heart"
+              color={theme.colors.white}
+              size={26}
+              style={{ marginHorizontal: 10 }}
+              onPress={() => navigation.navigate("Favorites")}
+            />
+          ),
+        };
+      }}
+    />
+    <HomeFlow.Screen
+      name="MovieDetails"
+      component={MovieDetails}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <HomeFlow.Screen
+      name="Favorites"
+      component={FavoritesScreen}
+      options={({ navigation }) => {
+        return {
+          headerTitle: strings("favorites"),
+          headerStyle: {
+            backgroundColor: theme.colors.accent,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: theme.colors.white,
+          },
+          headerLeft: () => (
+            <MaterialCommunityIcons
+              name="chevron-left"
+              color={theme.colors.white}
+              size={35}
+              style={{ paddingHorizontal: 10 }}
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerRight: () => <View />,
+        };
+      }}
+    />
+  </HomeFlow.Navigator>
+);
+
+export default HomeNavigation;
