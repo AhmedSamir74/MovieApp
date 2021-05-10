3585ba63abaafc480072cd27b0dde1007e8bcfa1
R9 Mobile dev <asamir@road9media.com>
2021-05-11 01:01:15 +0200

    add localiztion for english and arabic languages

---
+++ src/localization/en.ts
@@ -0,0 +1,27 @@
+const en = {
+  //General
+  appName: "TMDB Movies",
+  getStarted: "skip",
+  pickPoster: "Pick a poster",
+  title: "Title",
+  overview: "Overview",
+  date: "Date",
+  submit: "Submit",
+  moviesHome: "Movies Home",
+  emptyList: "Your Movies List Is Empty",
+  addMovie: "Add Movie",
+  confirm: "Confirm",
+  cancel: "Cancel",
+  voteAverage: "Vote average",
+  language: "Language",
+  genres: "Genres",
+  productionCompanies: "Production Companies",
+  favorites: "My Favorites",
+  search: "Search",
+  deleteHint: "Delete the movie from your favorites ?",
+  favListEmpty: "Your favorites list is empty",
+  startAddingNow: "start Adding now",
+  addedToFav: "Movie added to favorites",
+};
+
+export default en;
