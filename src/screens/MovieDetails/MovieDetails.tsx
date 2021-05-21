c1cb5b990c6b98e471a54b88e8753855eaf73cfa
R9 Mobile dev <asamir@road9media.com>
2021-05-21 17:45:03 +0200

    apply react query on movie details screen

--- src/screens/MovieDetails/MovieDetails.tsx
+++ src/screens/MovieDetails/MovieDetails.tsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   ActivityIndicator,
   Animated,
   FlatList,
   Image,
+  LogBox,
   ScrollView,
   StatusBar,
   Text,
@@ -22,130 +23,77 @@ import {
   removeFromFavorite,
 } from "../../services/redux/actions";
 import AnimatedToast from "../../components/UI/AnimatedToast/AnimatedToast";
+import { useNavigation } from "@react-navigation/native";
+import { useQuery } from "react-query";
+import APIKit from "../../services/axios/axios";
 
+LogBox.ignoreLogs(["etting a timer for a long period of time"]);
 const LoaderComp = () => {
   return (
     <View style={styles.loaderCont}>
-      <ActivityIndicator color={theme.colors.primary} size="large" />
+      <ActivityIndicator color={theme.colors.green} size="large" />
     </View>
   );
 };
 
-class MovieDetails extends Component<any> {
-  moviesManager: MoviesManager;
-  toastFadeAnimation: Animated.Value;
-  animatedToast: any;
+const MovieDetails = (props: any) => {
+  let { id } = props.route.params;
+  const { navigate, goBack } = useNavigation();
+  let animatedToast: any = useRef();
+  const [isFavorite, setIsFavorite] = useState(false);
+  const { data: movie, status, error, isLoading: requestLoading } = useQuery(
+    `movieDetails-${id}`,
+    async () => {
+      let { data: response } = await APIKit.get(`movie/${id}`);
+      return response;
+    }
+  );
+  if (status == "error") {
+    animatedToast?.showToast(error?.message);
+  }
 
-  state: {
-    requestLoading: boolean;
-    movie: IMovie | null;
-    isFavorite: boolean;
-    movieCredits: [];
+  const {
+    data: movieCredits,
+    status: creditsStatus,
+    error: creditsError,
+    isLoading: creditsLoading,
+  } = useQuery(`movieCredits-${id}`, async () => {
+    let { data: response } = await APIKit.get(`movie/${id}/credits`);
+    return response?.cast || [];
+  });
+
+  if (creditsStatus == "error") {
+    animatedToast?.showToast(creditsError?.message);
+  }
+
+  useEffect(() => {
+    checkInFavorite();
+  }, []);
+
+  const checkInFavorite = async () => {
+    let isMovieInFavorites = props.favorites.findIndex(
+      (mov: IMovie) => mov.id == id
+    );
+    if (isMovieInFavorites != -1) {
+      setIsFavorite(true);
+    }
   };
-  constructor(props: any) {
-    super(props);
-    this.moviesManager = new MoviesManager();
-    this.toastFadeAnimation = new Animated.Value(-200);
 
-    this.state = {
-      requestLoading: true,
-      movie: null,
-      isFavorite: false,
-      movieCredits: [],
-    };
-  }
+  const toastFadeAnimation = new Animated.Value(-200);
 
-  componentDidMount() {
-    this.getMovieDetails();
-    this.getMovieCredits();
-  }
-
-  async getMovieDetails() {
-    let { id } = this.props.route.params;
-    const { status, data } = await this.moviesManager.getMovieDetails(id);
-
-    if (status) {
-      this.setState({
-        movie: data,
-        requestLoading: false,
-      });
-    } else {
-      this.setState({
-        movie: [],
-        requestLoading: false,
-      });
-      this.animatedToast.showToast(data);
-    }
-    let isMovieInFavorites = this.props.favorites.findIndex(
-      (movie: IMovie) => movie.id == id
-    );
-    if (isMovieInFavorites != -1) {
-      this.setState({ isFavorite: true });
-    }
-  }
-  async getMovieCredits() {
-    let { id } = this.props.route.params;
-    const { status, data } = await this.moviesManager.getMovieCredits(id);
-
-    if (status) {
-      this.setState({
-        movieCredits: data,
-        requestLoading: false,
-      });
-    } else {
-      this.setState({
-        movieCredits: [],
-        requestLoading: false,
-      });
-      this.animatedToast.showToast(data);
-    }
-    let isMovieInFavorites = this.props.favorites.findIndex(
-      (movie: IMovie) => movie.id == id
-    );
-    if (isMovieInFavorites != -1) {
-      this.setState({ isFavorite: true });
-    }
-  }
-
-  showAnimatedToast() {
-    Animated.timing(this.toastFadeAnimation, {
-      toValue: 0,
-      duration: 800,
-      useNativeDriver: true,
-    }).start(() => {
-      setTimeout(() => {
-        Animated.timing(this.toastFadeAnimation, {
-          toValue: -200,
-          duration: 500,
-          useNativeDriver: true,
-        }).start();
-      }, 3000);
-    });
-  }
-
-  onFavoritePress() {
-    this.setState({ isFavorite: !this.state.isFavorite });
-    if (this.state.isFavorite) {
-      this.props.removeFromFavorites(this.state.movie?.id);
-    } else {
-      this.props.addToFavorites(this.state.movie);
-      this.showAnimatedToast();
-    }
-  }
-
-  renderAnimatedToast() {
+  const renderAnimatedToast = () => {
     return (
       <Animated.View
         style={[
           styles.toastCont,
           {
-            transform: [{ translateY: this.toastFadeAnimation }],
+            transform: [{ translateY: toastFadeAnimation }],
           },
         ]}
       >
         <TouchableOpacity
           activeOpacity={0.8}
-          onPress={() => this.props.navigation?.navigate("Favorites")}
+          onPress={() => navigate("Favorites")}
         >
           <View style={styles.touchableOpacity}>
             <View style={styles.toastImageCont}>
@@ -160,141 +108,160 @@ class MovieDetails extends Component<any> {
         </TouchableOpacity>
       </Animated.View>
     );
-  }
-  render() {
-    const { movie, requestLoading, movieCredits } = this.state;
-    return (
-      <View style={styles.layout}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
+  };
+
+  const showAnimatedToast = () => {
+    Animated.timing(toastFadeAnimation, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+    }).start(() => {
+      setTimeout(() => {
+        Animated.timing(toastFadeAnimation, {
+          toValue: -200,
+          duration: 500,
+          useNativeDriver: true,
+        }).start();
+      }, 3000);
+    });
+  };
+
+  const onFavoritePress = () => {
+    setIsFavorite((isFav) => !isFav);
+    if (isFavorite) {
+      props.removeFromFavorites(movie?.id);
+    } else {
+      props.addToFavorites(movie);
+      showAnimatedToast();
+    }
+  };
+  return (
+    <View style={styles.layout}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {renderAnimatedToast()}
+
+      <TouchableOpacity
+        style={styles.backIcon}
+        onPress={() => goBack()}
+        activeOpacity={0.6}
+      >
+        <MaterialCommunityIcons
+          name="chevron-left"
+          color={theme.colors.black}
+          size={40}
         />
+      </TouchableOpacity>
 
-        {this.renderAnimatedToast()}
+      <TouchableOpacity
+        style={[styles.backIcon, styles.favIcon]}
+        onPress={() => onFavoritePress()}
+        activeOpacity={0.6}
+      >
+        <MaterialCommunityIcons
+          name={isFavorite ? "heart" : "heart-outline"}
+          color={theme.colors.black}
+          size={30}
+        />
+      </TouchableOpacity>
+      {requestLoading ? (
+        <LoaderComp />
+      ) : (
+        movie && (
+          <ScrollView
+            style={styles.scrollView}
+            showsVerticalScrollIndicator={false}
+            scrollEventThrottle={16}
+          >
+            <View style={styles.imgCont}>
+              <Image
+                source={{
+                  uri: `https://www.themoviedb.org/t/p/w300_and_h300_bestv2/${
+                    movie.backdrop_path || movie.poster_path
+                  }`,
+                }}
+                style={styles.characterImg}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.infoCont}>
+              {/* TITLE */}
+              <Text style={styles.characterName}>{movie.title}</Text>
+              <Text style={styles.vote}>
+                {((movie.vote_average / 10) * 100).toFixed(0)}%
+              </Text>
 
-        <TouchableOpacity
-          style={styles.backIcon}
-          onPress={() => this.props.navigation.goBack()}
-          activeOpacity={0.6}
-        >
-          <MaterialCommunityIcons
-            name="chevron-left"
-            color={theme.colors.black}
-            size={40}
-          />
-        </TouchableOpacity>
+              {/* DESCRIPTION */}
+              {movie.overview ? (
+                <View style={styles.sectionCont}>
+                  <Text style={styles.sectionTitle}>{strings("overview")}</Text>
+                  <Text style={styles.sectionDesc}>{movie.overview}</Text>
+                </View>
+              ) : null}
 
-        <TouchableOpacity
-          style={[styles.backIcon, styles.favIcon]}
-          onPress={() => this.onFavoritePress()}
-          activeOpacity={0.6}
-        >
-          <MaterialCommunityIcons
-            name={this.state.isFavorite ? "heart" : "heart-outline"}
-            color={theme.colors.black}
-            size={30}
-          />
-        </TouchableOpacity>
-        {requestLoading ? (
-          <LoaderComp />
-        ) : (
-          movie && (
-            <ScrollView
-              style={styles.scrollView}
-              showsVerticalScrollIndicator={false}
-              scrollEventThrottle={16}
-            >
-              <View style={styles.imgCont}>
-                <Image
-                  source={{
-                    uri: `https://www.themoviedb.org/t/p/w300_and_h300_bestv2/${
-                      movie.backdrop_path || movie.poster_path
-                    }`,
-                  }}
-                  style={styles.characterImg}
-                  resizeMode="stretch"
-                />
-              </View>
-              <View style={styles.infoCont}>
-                {/* TITLE */}
-                <Text style={styles.characterName}>{movie.title}</Text>
-                <Text style={styles.vote}>
-                  {((movie.vote_average / 10) * 100).toFixed(0)}%
-                </Text>
-
-                {/* DESCRIPTION */}
-                {movie.overview ? (
-                  <View style={styles.sectionCont}>
-                    <Text style={styles.sectionTitle}>
-                      {strings("overview")}
-                    </Text>
-                    <Text style={styles.sectionDesc}>{movie.overview}</Text>
+              {/* GENERES */}
+              {movie.genres ? (
+                <View style={styles.sectionCont}>
+                  <Text style={styles.sectionTitle}>{strings("genres")}</Text>
+                  <View style={styles.genresCont}>
+                    {movie.genres.map((value: any) => (
+                      <Text style={styles.genres} key={value.id}>
+                        {value.name}
+                      </Text>
+                    ))}
                   </View>
-                ) : null}
+                </View>
+              ) : null}
 
-                {/* GENERES */}
-                {movie.genres ? (
-                  <View style={styles.sectionCont}>
-                    <Text style={styles.sectionTitle}>{strings("genres")}</Text>
-                    <View style={styles.genresCont}>
-                      {movie.genres.map((value) => (
-                        <Text style={styles.genres} key={value.id}>
-                          {value.name}
+              {/* VOTE */}
+              {movie.vote_average ? (
+                <View style={styles.sectionCont}>
+                  <Text style={styles.sectionTitle}>
+                    {strings("voteAverage")}
+                  </Text>
+                  <Text style={styles.sectionDesc}>
+                    {movie.vote_average}/10 ({movie.vote_count})
+                  </Text>
+                </View>
+              ) : null}
+
+              {/* CREDITS */}
+              {creditsStatus == "success" && movieCredits?.length != 0 && (
+                <View style={styles.sectionCont}>
+                  <Text style={styles.sectionTitle}>{strings("credits")}</Text>
+                  <FlatList
+                    horizontal
+                    data={movieCredits}
+                    renderItem={({ item }: any) => (
+                      <View style={styles.creditCont}>
+                        <Image
+                          source={{
+                            uri: `https://www.themoviedb.org/t/p/w138_and_h175_face${item.profile_path}`,
+                          }}
+                          style={styles.creditImg}
+                        />
+                        <Text style={[styles.sectionDesc, styles.creditName]}>
+                          {item.name}
                         </Text>
-                      ))}
-                    </View>
-                  </View>
-                ) : null}
-
-                {/* VOTE */}
-                {movie.vote_average ? (
-                  <View style={styles.sectionCont}>
-                    <Text style={styles.sectionTitle}>
-                      {strings("voteAverage")}
-                    </Text>
-                    <Text style={styles.sectionDesc}>
-                      {movie.vote_average}/10 ({movie.vote_count})
-                    </Text>
-                  </View>
-                ) : null}
-
-                {/* CREDITS */}
-                {movieCredits.length != 0 && (
-                  <View style={styles.sectionCont}>
-                    <Text style={styles.sectionTitle}>
-                      {strings("credits")}
-                    </Text>
-                    <FlatList
-                      horizontal
-                      data={movieCredits}
-                      renderItem={({ item }: any) => (
-                        <View style={styles.creditCont}>
-                          <Image
-                            source={{
-                              uri: `https://www.themoviedb.org/t/p/w138_and_h175_face${item.profile_path}`,
-                            }}
-                            style={styles.creditImg}
-                          />
-                          <Text style={[styles.sectionDesc, styles.creditName]}>
-                            {item.name}
-                          </Text>
-                        </View>
-                      )}
-                      keyExtractor={(item, index) => `${index}`}
-                      style={styles.creditList}
-                    />
-                  </View>
-                )}
-              </View>
-            </ScrollView>
-          )
-        )}
-        <AnimatedToast onRef={(ref: any) => (this.animatedToast = ref)} />
-      </View>
-    );
-  }
-}
+                      </View>
+                    )}
+                    keyExtractor={(item, index) => `${index}`}
+                    style={styles.creditList}
+                  />
+                </View>
+              )}
+            </View>
+          </ScrollView>
+        )
+      )}
+      <AnimatedToast onRef={(ref: any) => (animatedToast = ref)} />
+    </View>
+  );
+};
 
 const mapStateToProps = (state: any) => ({
   favorites: state.favorites.list,
